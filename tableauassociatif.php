726b51be0238d71b4b35e09d7fbe127170a6ea0d
diawando <abduldiawade777@gmail.com>
2023-11-08 18:14:39 +0000

    declaration du tableau associatif

---
+++ tableauassociatif.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html lang="fr">
+    <head>
+    </head>
+    <body>
+        <h2>Les tableaux php</h2>
+
+        <?php
+              $etudiants = ["nom" => "Aliou", "age" => 35, "adresse"=>"kipe"]
+        ?>
+    </body>
+</html>
